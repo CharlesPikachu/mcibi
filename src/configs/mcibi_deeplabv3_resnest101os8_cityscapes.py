399ae47b08c9e78adee4e7404f784890e6c185c7
CharlesPikachu <1159254961@qq.com>
2024-10-21 21:42:01 +0800

    be consistent with ssseg repo

--- src/configs/mcibi_deeplabv3_resnest101os8_cityscapes.py
+++ src/configs/mcibi_deeplabv3_resnest101os8_cityscapes.py
@@ -9,6 +9,8 @@ from .._base_ import DATASET_CFG_CITYSCAPES_512x1024, DATALOADER_CFG_BS16
 SEGMENTOR_CFG = copy.deepcopy(SEGMENTOR_CFG)
 # modify dataset config
 SEGMENTOR_CFG['dataset'] = DATASET_CFG_CITYSCAPES_512x1024.copy()
+SEGMENTOR_CFG['dataset']['train']['set'] = 'trainval'
+SEGMENTOR_CFG['dataset']['test']['set'] = 'test'
 # modify dataloader config
 SEGMENTOR_CFG['dataloader'] = DATALOADER_CFG_BS16.copy()
 # modify scheduler config
@@ -20,6 +22,15 @@ SEGMENTOR_CFG['backbone'] = {
 }
 SEGMENTOR_CFG['head']['use_loss'] = False
 SEGMENTOR_CFG['head']['update_cfg']['momentum_cfg']['base_lr'] = 0.01 * 0.9
+SEGMENTOR_CFG['inference'] = {
+    'mode': 'slide',
+    'opts': {'cropsize': (1024, 512), 'stride': (341, 341)}, 
+    'tricks': {
+        'multiscale': [0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        'flip': True,
+        'use_probs_before_resize': True,
+    }
+}
 SEGMENTOR_CFG['work_dir'] = os.path.split(__file__)[-1].split('.')[0]
 SEGMENTOR_CFG['evaluate_results_filename'] = f"{os.path.split(__file__)[-1].split('.')[0]}.pkl"
 SEGMENTOR_CFG['logger_handle_cfg']['logfilepath'] = os.path.join(SEGMENTOR_CFG['work_dir'], f"{os.path.split(__file__)[-1].split('.')[0]}.log")
